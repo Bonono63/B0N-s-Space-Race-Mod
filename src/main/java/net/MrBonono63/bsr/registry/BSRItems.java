b2d69e1492f8ebe07f0b94c4cb9e4d8b4a3f7f2b
Bonono63 <bonoria63@gmail.com>
2020-01-31 17:32:46 -0500

    fixed Asteroid Iron block texture

--- src/main/java/net/MrBonono63/bsr/registry/BSRItems.java
+++ src/main/java/net/MrBonono63/bsr/registry/BSRItems.java
@@ -1,13 +1,12 @@
 package net.MrBonono63.bsr.registry;
 
 import net.minecraft.item.Item;
-import net.minecraft.item.ToolMaterial;
 import net.minecraft.util.registry.Registry;
 import net.MrBonono63.bsr.BSR;
 
 public class BSRItems {
 
-    public static final Item OXYGEN_GAS_CANISTER = register("oxygen_gas_canister", new Item(newSettings()));
+    public static final Item OXYGEN_GAS_BUCKET = register("oxygen_gas_bucket", new Item(newSettings()));
 
     private BSRItems() {
         //NO-OP
